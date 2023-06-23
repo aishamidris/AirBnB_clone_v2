3108c92ae65cb399052e3fa4a3ad0ddc6a1bb519
aishamidris <aishamuhammadidris92@gmail.com>
2023-06-23 06:36:12 -0700

    aibnb clone

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
+
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative.base
+from sqlalchemy import foreignkey, string, column, DateTime 
 
+Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+    id = column(string(60), primary_key=True, nullable=False)
+    created_at =column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = column(DateTime, nullable=False, datetime.utcnow())
+    
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -31,6 +40,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -41,4 +51,12 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        try:
+            del dictionary['_sa_instance_state']
+        except Exception:
+            pass
         return dictionary
+    
+    def delete(self)
+    """delete the current instance from the storage"""
+    storage.delete(self)
--- models/city.py
+++ models/city.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import column, foreignkey, string
 
-
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+    state_id = columin(string(60), foreignkey(states.id),  nullable=False
+    name = columin(string(128), nullable=False
